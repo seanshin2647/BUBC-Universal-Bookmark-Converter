77dd62b0b1eba22bbc134bc8976f24975cc66fe2
JoshYoung2693 <56082102+JoshYoung2693@users.noreply.github.com>
2020-10-29 18:31:59 -0600

    Update Main.java

--- src/main/java/com/bubcdevs/main/Main.java
+++ src/main/java/com/bubcdevs/main/Main.java
@@ -4,5 +4,9 @@ public class Main {
     
     public static void main(String[] args) {
         Window window = Window.getWindow();
+        Input input = new Input();
+        String[] filePathList = input.input();
+        operatingSystem = filePathList[0];
+        filePath = filePathList[1];
     }
 }
