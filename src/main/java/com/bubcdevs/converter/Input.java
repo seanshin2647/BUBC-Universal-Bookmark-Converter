4000a8b5978588f9238517ee6994da74628b6e6f
Sean Shin <seanshin2647@gmail.com>
2020-11-29 02:11:49 -0700

    Refactor Input.java to be used as a component

--- src/main/java/com/bubcdevs/converter/Input.java
+++ src/main/java/com/bubcdevs/converter/Input.java
@@ -4,32 +4,28 @@ import java.util.Scanner;
 
 public class Input {
     Scanner scanner;
+    String filePath;
+    String operatingSystem;
 
     public String[] input() {
-        Input input = new Input();
-        input.createTheScanner();
-        String filePath = input.getFilePath();
-        String operatingSystem = input.verify(filePath);
-        input.currentStatus(operatingSystem);
+        scanner = new Scanner(System.in);
+        filePath = getFilePath();
+        operatingSystem = verify(filePath);
+        currentStatus(operatingSystem);
 
         return new String[]{operatingSystem, filePath};
 
     }
 
-    public void createTheScanner(){
-        scanner = new Scanner(System.in);
-    }
-
     public String getFilePath() {
         // When someone drags a file into a terminal window, it will automatically paste the file path.
         System.out.println("Drag and drop your \".db\" file into this window and press enter.");
-        String filePath = scanner.nextLine();
+        filePath = scanner.nextLine();
         return filePath;
     }
 
     // Verify that the input is valid and return what OS they are using.
     public String verify(String filePath) {
-        String operatingSystem;
 
         // Unix based filesystems always begin with a "/"
         if (filePath.startsWith("/")) {
@@ -55,7 +51,7 @@ public class Input {
         if (operatingSystem == "Invalid com.bubcdevs.converter.Input") {
             System.out.println("Invalid input or failed process. Try again.");
         } else {
-            System.out.println("Valid input. Process successful. You are using a "+operatingSystem+" based filesystem.");
+            System.out.println("Valid input. Process successful. You are using a " + operatingSystem + " based filesystem.");
         }
     }
 
